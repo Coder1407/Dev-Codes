191d4c567e9ae071d2e3624924db64be352a0ab8
Coder1407 <121633636+Coder1407@users.noreply.github.com>
2024-01-23 23:52:30 +0530

    Added java file

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    void bubbleSort() {
+	//
+    }
+    void quickSort {
+	//
+    }
+    void mergeSort {
+	//
+    }
+    public static void main(String args[]) {
+	No. of servers = 20;
+	int a=4, b=5;
+	int c = a+b;
+	System.out.prinltn("Hello World"+c);
+    }
+}
