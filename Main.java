56ad679fdf37833add586bf5e3fe190cadde63d8
Coder1407 <121633636+Coder1407@users.noreply.github.com>
2024-01-23 23:55:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,7 +10,7 @@ public class Main {
 	//
     }
     public static void main(String args[]) {
-	No. of servers = 20;
+	No. of servers = 15;
 	int a=4, b=5;
 	int c = a+b;
 	System.out.prinltn("Hello World"+c);
